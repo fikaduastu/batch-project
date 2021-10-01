57fcfcac17a8fc39d8cb3bc0cb586d75495c824a
fikaduastu <1988.fikadufilipos@gmail.com>
2021-10-01 10:59:27 -0500

    admin registration is working with out security

--- src/main/java/com/batchproject/repository/AdminRepository.java
+++ src/main/java/com/batchproject/repository/AdminRepository.java
@@ -1,5 +1,7 @@
 package com.batchproject.repository;
 
+
+
 import com.batchproject.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
